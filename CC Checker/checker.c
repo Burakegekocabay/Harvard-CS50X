46f1f9cd4e862406db8d61fa7d23ed3adba89060
burakegekocabay <burakegekocabay@outlook.com>
2024-06-29 01:25:47 +0300

    Fixed American Express Problem

--- CC Checker/checker.c
+++ CC Checker/checker.c
@@ -1,38 +1,41 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char const *argv[])
 {
     char input[30];
     printf("Enter your CC number: ");
     scanf("%s", &input);
-    
-    int i=0;
+    int i=strlen(input);
     int total=0;
-    while(input[i] != '\0')
+    int control=0;
+
+    while(i != 0)
     {
-        if (i%2==0)
+        if (control)
         {
-            if (input[i]-'0'<5)
+            if (input[i-1]-'0'<5)
             {
-                total= total + 2*(input[i]-'0');
+                total= total + 2*(input[i-1]-'0');
             }
 
             else
             {
-                int temp = 2*(input[i]-'0');
+                int temp = 2*(input[i-1]-'0');
                 total=total+(temp/10)+(temp%10);
             }
         }
         else
-            total = total+(input[i]-'0');
+            total = total+(input[i-1]-'0');
         
         
-        i++; 
+        i--;
+        control = !control;
     };
 
     if (total%10==0)
     {
-        if(input[0] == '2' || input[0] == '5')
+        if(input[0] == '2' || input[0] == 5)
         {
             printf("VALID : MasterCard \n");
         }
